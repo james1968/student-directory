1b399430485a47d7012adf90171cfab7d403ed6a
James <bakerje@mac.com>
2016-09-12 16:18:45 +0100

    updates for array and iteration

--- directory.rb
+++ directory.rb
@@ -1,19 +1,22 @@
-#first we print the list of students
+# lets put all the students into an array
+students = [
+  "Dr Hannibal Lecter",
+  "Darth Vadar",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the North",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+# and then print them
 puts "The students of Villains Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vadar"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked WItch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#finally we print the total number of students
-print "Overall, we have "
-#it's important that print() doesn't add new line characters
-print 11
-puts " great students"
+puts "------------"
+students.each do |student|
+  puts student
+end
+# finally, we print the total
+puts "Overall, we have #{students.count} great students"
